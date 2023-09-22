44cfbdfc0cab934bc8ce788ac25c16df5dda9af9
xanderyeng <ckalex54@gmail.com>
2023-09-22 22:43:55 +0300

    Fixing the checker errors

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,12 @@
--- create database hbnb_dev_db
--- usage cat 1-create_database_if_missing.sql | mysql -hlocalhost -uroot -p
+-- Creating User and givig it permissions
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT USAGE ON *.*
+      TO 'hbnb_dev'@'localhost'
+      IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.*
+      TO 'hbnb_dev'@'localhost'
+      IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT SELECT ON performance_schema.*
+      TO 'hbnb_dev'@'localhost'
+      IDENTIFIED BY 'hbnb_dev_pwd';
 FLUSH PRIVILEGES;
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,7 +1,12 @@
--- create database hbnb_test_db
--- usage cat 1-create_database_if_missing.sql | mysql -hlocalhost -uroot -p
+-- creating test user and giving it permissions
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT USAGE ON *.*
+      TO 'hbnb_test'@'localhost'
+      IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.*
+      TO 'hbnb_test'@'localhost'
+      IDENTIFIED BY 'hbnb_test_pwd';
+GRANT SELECT ON performance_schema.*
+      TO 'hbnb_test'@'localhost'
+      IDENTIFIED BY 'hbnb_test_pwd';
 FLUSH PRIVILEGES;
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
